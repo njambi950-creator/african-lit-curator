ee09fa1f2cc1212e8cbc8e6b14cef27b79bfa9fa
Njambi <njambingugi950@gmail.com>
2026-05-07 10:40:03 +0300

    fix: improve validation for required fields in book form submission

--- code.js
+++ code.js
@@ -5,9 +5,14 @@ const bookForm = document.getElementById('book-form');
 if (bookForm) {
     bookForm.addEventListener('submit', function(e) {
         e.preventDefault(); // Stop page from refreshing
- 1. Get values from the input boxes
+//  1. Get values from the input boxes
         const title = document.getElementById('title').value;
         const author = document.getElementById('author').value;
         const genre = document.getElementById('genre').value;
         const review = document.getElementById('review').value;
         const coverUrl = document.getElementById('coverUrl').value;
+        // 2.Validation
+        if (title === "" || author === "") {
+            alert("Please fill in the required fields!");
+            return;
+        }
